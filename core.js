bca4466bbc7ebd534abc5e1ceb2132f2b7f6e0e8
Evgeny Blokhin <eb@tilde.pro>
2016-01-10 23:09:29 +0100

    First demo of search field functionality

--- core.js
+++ core.js
@@ -19,6 +19,8 @@ _gui.sortdisable = false;
 _gui.cwidth = 0;
 _gui.connattempts = 0;
 _gui.max_connattempt = 2;
+_gui.searchables = []; // all categs
+_gui.impliables = []; // non-faceted, but selected categs
 _gui.plots = [];
 _gui.mendeleev = {};
 _gui.mendeleev_table = ['X', 'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr'];
@@ -51,6 +53,14 @@ if (!console) console = {log: function(){}};
 
 String.prototype.startswith = function(prefix){
     return this.indexOf(prefix) === 0;
+}
+
+String.prototype.endswith = function(searchString, position){
+    var subjectString = this.toString();
+    if (position === undefined || position > subjectString.length) position = subjectString.length;
+    position -= searchString.length;
+    var lastIndex = subjectString.indexOf(searchString, position);
+    return lastIndex !== -1 && lastIndex === position;
 }
 
 // SockJS
@@ -294,6 +304,7 @@ function defaultize_tags(){
     $('a.mdtag').addClass('visibletag');
     //$('div.tagrow').show();
     //$('#initbox').hide();
+    _gui.impliables = [];
 }
 
 function defaultize_sliders(){
@@ -301,6 +312,22 @@ function defaultize_sliders(){
         var min = parseFloat($(this).attr('min')), max = parseFloat($(this).attr('max'));
         $(this).noUiSlider({ start: [min, max] }, true);
     });
+}
+
+function direct_search(){
+    if (!$('#search_field').val().length) return notify('Please, choose the topic(s)');
+    var term = $('#search_field').val().toLowerCase();
+    //if (term.endswith(', ')) // FIXME
+    for (i=0; i<_gui.searchables.length; i++){
+        if (_gui.searchables[i][1].toLowerCase() == term) return __send('tags', {tids: [_gui.searchables[i][0]]});
+    }
+    notify('Nothing found.');
+}
+
+function clear_tags(){
+    $('#search_field').val('');
+    defaultize_tags();
+    add_tag_expanders();
 }
 
 function gather_tags(myself){
@@ -316,6 +343,8 @@ function gather_tags(myself){
     $('#splashscreen').find('a.activetag').each(function(){
         found_tags.push( $(this).attr('rel') );
     });
+
+    if (_gui.impliables.length) found_tags.push.apply(found_tags, _gui.impliables); // .extend
 
     return found_tags;
 }
@@ -394,6 +423,226 @@ function clean_plots(){
 function align_page(){
     (_gui.cwidth < 1280) ? $('div.supercat').css('width', '98%') : $('div.supercat').css('width', '49%');
 } // test width!
+/**
+*
+*
+* *********************************************************************************************************************
+*
+*/
+/* JavaScript autoComplete v1.0.3 (MIT) by Simon Steinberger / Pixabay
+https://github.com/Pixabay/JavaScript-autoComplete */
+var autoComplete = (function(){
+    // "use strict";
+    function autoComplete(options){
+        if (!document.querySelector) return;
+
+        // helpers
+        function hasClass(el, className){ return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className); }
+
+        function addEvent(el, type, handler){
+            if (el.attachEvent) el.attachEvent('on'+type, handler); else el.addEventListener(type, handler);
+        }
+        function removeEvent(el, type, handler){
+            // if (el.removeEventListener) not working in IE11
+            if (el.detachEvent) el.detachEvent('on'+type, handler); else el.removeEventListener(type, handler);
+        }
+        function live(elClass, event, cb, context){
+            addEvent(context || document, event, function(e){
+                var found, el = e.target || e.srcElement;
+                while (el && !(found = hasClass(el, elClass))) el = el.parentElement;
+                if (found) cb.call(el, e);
+            });
+        }
+
+        var o = {
+            selector: 0,
+            source: 0,
+            minChars: 3,
+            delay: 150,
+            cache: 1,
+            menuClass: '',
+            renderItem: function (item, search){
+                // escape special characters
+                search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+                var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+                return '<div class="autocomplete-suggestion" data-val="' + item + '">' + item.replace(re, "<b>$1</b>") + '</div>';
+            },
+            onSelect: function(e, term, item){}
+        };
+        for (var k in options) { if (options.hasOwnProperty(k)) o[k] = options[k]; }
+
+        // init
+        var elems = typeof o.selector == 'object' ? [o.selector] : document.querySelectorAll(o.selector);
+        for (var i=0; i<elems.length; i++) {
+            var that = elems[i];
+
+            // create suggestions container "sc"
+            that.sc = document.createElement('div');
+            that.sc.className = 'autocomplete-suggestions '+o.menuClass;
+
+            that.autocompleteAttr = that.getAttribute('autocomplete');
+            that.setAttribute('autocomplete', 'off');
+            that.cache = {};
+            that.last_val = '';
+
+            that.updateSC = function(resize, next){
+                var rect = that.getBoundingClientRect();
+                that.sc.style.left = rect.left + (window.pageXOffset || document.documentElement.scrollLeft) + 'px';
+                that.sc.style.top = rect.bottom + (window.pageYOffset || document.documentElement.scrollTop) + 1 + 'px';
+                that.sc.style.width = rect.right - rect.left + 'px'; // outerWidth
+                if (!resize) {
+                    that.sc.style.display = 'block';
+                    if (!that.sc.maxHeight) { that.sc.maxHeight = parseInt((window.getComputedStyle ? getComputedStyle(that.sc, null) : that.sc.currentStyle).maxHeight); }
+                    if (!that.sc.suggestionHeight) that.sc.suggestionHeight = that.sc.querySelector('.autocomplete-suggestion').offsetHeight;
+                    if (that.sc.suggestionHeight)
+                        if (!next) that.sc.scrollTop = 0;
+                        else {
+                            var scrTop = that.sc.scrollTop, selTop = next.getBoundingClientRect().top - that.sc.getBoundingClientRect().top;
+                            if (selTop + that.sc.suggestionHeight - that.sc.maxHeight > 0)
+                                that.sc.scrollTop = selTop + that.sc.suggestionHeight + scrTop - that.sc.maxHeight;
+                            else if (selTop < 0)
+                                that.sc.scrollTop = selTop + scrTop;
+                        }
+                }
+            }
+            addEvent(window, 'resize', that.updateSC);
+            document.body.appendChild(that.sc);
+
+            live('autocomplete-suggestion', 'mouseleave', function(e){
+                var sel = that.sc.querySelector('.autocomplete-suggestion.selected');
+                if (sel) setTimeout(function(){ sel.className = sel.className.replace('selected', ''); }, 20);
+            }, that.sc);
+
+            live('autocomplete-suggestion', 'mouseover', function(e){
+                var sel = that.sc.querySelector('.autocomplete-suggestion.selected');
+                if (sel) sel.className = sel.className.replace('selected', '');
+                this.className += ' selected';
+            }, that.sc);
+
+            live('autocomplete-suggestion', 'mousedown', function(e){
+                if (hasClass(this, 'autocomplete-suggestion')) { // else outside click
+                    var v = this.getAttribute('data-val');
+                    that.value = v;
+                    o.onSelect(e, v, this);
+                    that.sc.style.display = 'none';
+                }
+            }, that.sc);
+
+            that.blurHandler = function(){
+                try { var over_sb = document.querySelector('.autocomplete-suggestions:hover'); } catch(e){ var over_sb = 0; }
+                if (!over_sb) {
+                    that.last_val = that.value;
+                    that.sc.style.display = 'none';
+                    setTimeout(function(){ that.sc.style.display = 'none'; }, 350); // hide suggestions on fast input
+                } else if (that !== document.activeElement) setTimeout(function(){ that.focus(); }, 20);
+            };
+            addEvent(that, 'blur', that.blurHandler);
+
+            var suggest = function(data){
+                var val = that.value;
+                that.cache[val] = data;
+                if (data.length && val.length >= o.minChars) {
+                    var s = '';
+                    for (var i=0;i<data.length;i++) s += o.renderItem(data[i], val);
+                    that.sc.innerHTML = s;
+                    that.updateSC(0);
+                }
+                else
+                    that.sc.style.display = 'none';
+            }
+
+            that.keydownHandler = function(e){
+                var key = window.event ? e.keyCode : e.which;
+                // down (40), up (38)
+                if ((key == 40 || key == 38) && that.sc.innerHTML) {
+                    var next, sel = that.sc.querySelector('.autocomplete-suggestion.selected');
+                    if (!sel) {
+                        next = (key == 40) ? that.sc.querySelector('.autocomplete-suggestion') : that.sc.childNodes[that.sc.childNodes.length - 1]; // first : last
+                        next.className += ' selected';
+                        that.value = next.getAttribute('data-val');
+                    } else {
+                        next = (key == 40) ? sel.nextSibling : sel.previousSibling;
+                        if (next) {
+                            sel.className = sel.className.replace('selected', '');
+                            next.className += ' selected';
+                            that.value = next.getAttribute('data-val');
+                        }
+                        else { sel.className = sel.className.replace('selected', ''); that.value = that.last_val; next = 0; }
+                    }
+                    that.updateSC(0, next);
+                    return false;
+                }
+                // esc
+                else if (key == 27) { that.value = that.last_val; that.sc.style.display = 'none'; }
+                // enter
+                else if (key == 13 || key == 9) {
+                    var sel = that.sc.querySelector('.autocomplete-suggestion.selected');
+                    if (sel && that.sc.style.display != 'none') { o.onSelect(e, sel.getAttribute('data-val'), sel); setTimeout(function(){ that.sc.style.display = 'none'; }, 20); }
+                    else { // direct search : FIXME
+                        $('#init_trigger').trigger('click'); // FIXME
+                    }
+                }
+                // deletion : FIXME
+                else if (key == 8 || key == 46){
+                    window.clear_tags();
+                }
+            };
+            addEvent(that, 'keydown', that.keydownHandler);
+
+            that.keyupHandler = function(e){
+                var key = window.event ? e.keyCode : e.which;
+                if (!key || (key < 35 || key > 40) && key != 13 && key != 27) {
+                    var val = that.value;
+                    if (val.length >= o.minChars) {
+                        if (val != that.last_val) {
+                            that.last_val = val;
+                            clearTimeout(that.timer);
+                            if (o.cache) {
+                                if (val in that.cache) { suggest(that.cache[val]); return; }
+                                // no requests if previous suggestions were empty
+                                for (var i=1; i<val.length-o.minChars; i++) {
+                                    var part = val.slice(0, val.length-i);
+                                    if (part in that.cache && !that.cache[part].length) { suggest([]); return; }
+                                }
+                            }
+                            that.timer = setTimeout(function(){ o.source(val, suggest) }, o.delay);
+                        }
+                    } else {
+                        that.last_val = val;
+                        that.sc.style.display = 'none';
+                    }
+                }
+            };
+            addEvent(that, 'keyup', that.keyupHandler);
+
+            that.focusHandler = function(e){
+                that.last_val = '\n';
+                that.keyupHandler(e)
+            };
+            if (!o.minChars) addEvent(that, 'focus', that.focusHandler);
+        }
+
+        // public destroy method
+        this.destroy = function(){
+            for (var i=0; i<elems.length; i++) {
+                var that = elems[i];
+                removeEvent(window, 'resize', that.updateSC);
+                removeEvent(that, 'blur', that.blurHandler);
+                removeEvent(that, 'focus', that.focusHandler);
+                removeEvent(that, 'keydown', that.keydownHandler);
+                removeEvent(that, 'keyup', that.keyupHandler);
+                if (that.autocompleteAttr)
+                    that.setAttribute('autocomplete', that.autocompleteAttr);
+                else
+                    that.removeAttribute('autocomplete');
+                document.body.removeChild(that.sc);
+                that = null;
+            }
+        };
+    }
+    return autoComplete;
+})();
+(function(){ window.autoComplete = autoComplete })();
 /**
 *
 *
@@ -688,6 +937,8 @@ function resp__browse(req, data){
 function resp__tags(req, data){
     if (req.tids && req.tids.length){
         // UPDATE AVAILABLE TAGS
+        var tag_names = [];
+        _gui.impliables = [];
         //$('#initbox').show();
         $('a.taglink').removeClass('visibletag'); // reset shown tags
 
@@ -695,8 +946,14 @@ function resp__tags(req, data){
             $('a._tag'+i).addClass('visibletag');
         });
         $.each(req.tids, function(n, i){
-            $('a.taglink._tag'+i).addClass('visibletag activetag');
+            var $that = $('a.taglink._tag'+i);
+            if ($that.length) $that.addClass('visibletag activetag');
+            else _gui.impliables.push(i);
+            for (j=0; j<_gui.searchables.length; j++){
+                if (_gui.searchables[j][0] == i) tag_names.push(_gui.searchables[j][1]);
+            }
         });
+        $('#search_field').val(tag_names.join(', ') + ', ');
     } else {
         // BUILD TAGS FROM SCRATCH
         var tags_html = '';
@@ -737,6 +994,8 @@ function resp__tags(req, data){
             tags_html += '<div id="supercat_'+i.id+'" class="supercat"> <div class="supercat_title">'+i.category.charAt(0).toUpperCase() + i.category.slice(1)+'</div> <div class="supercat_content">' + i.html_pocket + '</div> </div>';
         });
 
+        _gui.searchables = data.searchables || [];
+
         if (!tags_html.length) tags_html = '<div class=center>DB is empty!</div>';
         $('#splashscreen').empty().append(tags_html);
 
@@ -773,6 +1032,7 @@ function resp__tags(req, data){
     if (!$.isEmptyObject(data)) $('#splashscreen').show();
 
     add_tag_expanders();
+    $('#search_field').focus();
 }
 
 function resp__summary(req, data){
@@ -874,6 +1134,24 @@ $(document).ready(function(){
     $('#notifybox').hide();
 
     window.onhashchange = url_redraw_react;
+
+    var search_field = new autoComplete({selector: '#search_field', minChars: 2, source: function(term, suggest){
+        term = term.toLowerCase();
+        var suggestions = [];
+        for (i=0; i<_gui.searchables.length; i++){
+            if (~_gui.searchables[i][1].toLowerCase().indexOf(term)) suggestions.push(_gui.searchables[i]);
+        }
+        suggest(suggestions);
+    },
+    renderItem: function (item, search){
+        search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+        var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+        return '<div class="autocomplete-suggestion" data-tid="' + item[0] + '" data-val="' + item[1] + '">' + item[1].replace(re, "<b>$1</b>") + '</div>';
+    },
+    onSelect: function(e, term, item){
+        var tid = parseInt($(item).attr('data-tid')); // FIXME prevent filling in
+        __send('tags', {tids: [tid]});
+    }});
 
     var last_servers_html = '';
     $.each($.jStorage.index(), function(n, i){
@@ -1022,8 +1300,6 @@ $(document).ready(function(){
     // DATABROWSER MENU
     $('#noclass_trigger').click(function(){
         _gui.req_stack = [];
-        //defaultize_tags();
-        //defaultize_sliders();
         window.location.hash = '#start';
     });
     /*$('#closeobj_trigger').click(function(){
@@ -1049,12 +1325,13 @@ $(document).ready(function(){
             window.location.hash = '#browse/' + tags.join('+');
         } else if (_gui.conditions){
             __send('browse', {conditions: _gui.conditions});
-        } else notify('Please, choose the topic(s)');
+        } else direct_search();
     });
 
     // SPLASHSCREEN CANCEL TAG QUERY
     $('#cnl_trigger').click(function(){
         _gui.conditions = false;
+        $('#search_field').val('');
         defaultize_tags();
         defaultize_sliders();
         add_tag_expanders();
@@ -1175,6 +1452,7 @@ $(document).ready(function(){
         if (tags.length){
             __send('tags', {tids: tags});
         } else {
+            $('#search_field').val('');
             defaultize_tags();
             add_tag_expanders();
         }
